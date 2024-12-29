79f60270de63464de6297ad000851977b5e9e639
오성식 <blackvrice@naver.com>
2024-12-29 22:16:30 +0900

    삭제

--- WindowsProject1/WindowsProject1/Character.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Position.h"
-
-class Character : Position
-{ 
-	
-};
-
--- WindowsProject1/WindowsProject1/WindowsProject1.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "resource.h"
-#include <iostream>
-#include <string>
-#include <windows.h>
-#include <tchar.h>
